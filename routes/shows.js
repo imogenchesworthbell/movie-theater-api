dc7b23b478df2e37728b48d4c3508bb024f47027
imogenchesworthbell <imogen.chesworthbell@barclays.com>
2024-10-18 15:09:35 +0100

    some more CRUD

--- routes/shows.js
+++ routes/shows.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const showRouter = express.Router();
-const Show = require("../models/Show");
+const { Show, User } = require("../models");
 const { check, validationResult } = require("express-validator");
 
 // GET all shows
@@ -11,29 +11,51 @@ showRouter.get("/", async (request, response) => {
 
 // GET one show
 showRouter.get("/:id", async (request, response) => {
-    const number = request.params.id
-    const show = await Show.findByPk(number)
-    response.json(show)
-})
+  const number = request.params.id;
+  const show = await Show.findByPk(number);
+  response.json(show);
+});
 
 // GET all users who watched a show
-showRouter.get("/", async (request, response) => {})
+showRouter.get("/:id/users", async (request, response) => {
+  const showId = request.params.id;
+  if (!(await Show.findByPk(showId))) {
+    response.json({ error: "Show not found" });
+  }
+  const show = await Show.findByPk(showId, {
+    include: {
+      model: User,
+      through: { attributes: [] },
+    },
+  });
+  response.json(show.users);
+});
 
-// PUT update the available property of a show
-showRouter.put("/", async (request, response) => {})
+//PUT - update 1 show
+showRouter.put("/:id", async (request, response) => {
+    const updatedShows = await Show.update(request.body, {where: {id: request.params.id}});
+    let shows = await Show.findAll()
+    response.json(shows);
+})
 
-//POST - Use server-side validation in your routes to ensure that:
-//The title of a show must be a maximum of 25 characters
-showRouter.post("/", async (request, response) => {})
+// // PUT - update the available property of a show
+showRouter.put("/:id/available", async (request, response) => {
+    const updatedShows = await Show.update(request.body, {where: {id: request.params.id}});
+    let shows = await Show.findAll()
+    response.json(shows);
+})
 
 // DELETE a show
 showRouter.delete("/:id", async (request, response) => {
-    const deletedShow = await Show.destroy({where: {id: request.params.id}})
-    const shows = await Show.findAll()
-    response.json(shows)
-})
+  const deletedShow = await Show.destroy({ where: { id: request.params.id } });
+  const shows = await Show.findAll();
+  response.json(shows);
+});
 
 // GET shows of a particular genre (genre in req.query)
-showRouter.get("/", async (request, response) => {})
+showRouter.get("/", async (request, response) => {});
 
+//POST - Use server-side validation in your routes to ensure that:
+//The title of a show must be a maximum of 25 characters
+showRouter.post("/", async (request, response) => {});
 module.exports = showRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const userRouter = express.Router();
-const User = require("../models/User");
+const { User, Show } = require("../models");
 const { check, validationResult } = require("express-validator");
 
 // GET all users
@@ -11,26 +11,64 @@ userRouter.get("/", async (request, response) => {
 
 // GET one user
 userRouter.get("/:id", async (request, response) => {
-    const number = request.params.id
-    const user = await User.findByPk(number)
-    response.json(user)
-})
+  const number = request.params.id;
+  const user = await User.findByPk(number);
+  response.json(user);
+});
 
 // GET all shows watched by a user (user id in req.params)
-userRouter.get("/", async (request, response) => {})
+userRouter.get("/:id/shows", async (request, response) => {
+  const userId = request.params.id;
+  if (!(await User.findByPk(userId))) {
+    response.json({ error: "User not found" });
+  }
+  const user = await User.findByPk(userId, {
+    include: {
+      model: Show,
+      through: { attributes: [] },
+    },
+  });
+  response.json(user.shows);
+});
 
-//POST - Use server-side validation in your routes to ensure that:
-//The username must be an email address hint
-userRouter.post("/", async (request, response) => {})
+//PUT - update 1 user
+userRouter.put("/:id", async (request, response) => {
+  const updatedUsers = await User.update(request.body, {
+    where: { id: request.params.id },
+  });
+  let users = await User.findAll();
+  response.json(users);
+});
 
 // PUT associate a user with a show they have watched
-userRouter.put("/", async (request, response) => {})
+userRouter.put("/:userId/shows/:showId", async (request, response) => {
+  const userId = request.params.userId;
+  const showId = request.params.showId;
+  const user = await User.findByPk(userId);
+  const show = await Show.findByPk(showId);
+  if (!user) {
+    response.json({ message: "User not found" });
+  } else if (!show) {
+    response.json({ message: "Show not found" });
+  } else {
+    await user.addShow(show);
+    const updatedUser = await User.findByPk(userId, { include: Show });
+    response.json(updatedUser);
+  }
+});
 
 //DELETE a user
 userRouter.delete("/:id", async (request, response) => {
-    const deletedUser = await User.destroy({where: {id: request.params.id}})
-    const users = await User.findAll()
-    response.json(users)
-})
+  const deletedUser = await User.destroy({ where: { id: request.params.id } });
+  const users = await User.findAll();
+  response.json(users);
+});
+
+//POST - Use server-side validation in your routes to ensure that:
+//The username must be an email address hint
+userRouter.post("/", async (request, response) => {});
+
+
+
 
 module.exports = userRouter;
